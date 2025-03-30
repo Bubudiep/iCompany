ad55c9402cec8a2dc8576c98f979a96d23d8ddf9
Huy <nqhuy0607@gmail.com>
2025-03-30 16:42:30 +0700

    done login

--- src/page/login.jsx
+++ src/page/login.jsx
@@ -2,11 +2,13 @@ import React, { useEffect, useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import app from "../components/app";
 import api from "../components/api";
-import { Button, message, notification, Popconfirm } from "antd";
+import "../assets/css/login.css";
+import { Input, Button, message, notification, Popconfirm } from "antd";
 const Login = () => {
   const [loading, setLoading] = useState(false);
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
   const [error, setError] = useState(null);
   const navigate = useNavigate();
   const author = import.meta.env.VITE_AUTHOR;
@@ -39,7 +41,7 @@ const Login = () => {
       api.send("unmaximize");
       api.send("resize", false);
     } catch (err) {
-      console.error("Not support electron!");
+      console.error("Not support electron!", err);
     }
   }, []);
 
@@ -76,23 +78,37 @@ const Login = () => {
       setLoading(false);
     }
   };
+
   return (
-    <div className="login-page">
-      <div className="video-container">
-        <div className="video-description">
-          <div className="box-sizing"></div>
-        </div>
+    <div className="login-page h-screen flex items-center justify-center">
+      {/* Left Section */}
+      <div className="video-container hidden md:block w-1/2 p-2 bg-gray-50">
+        <img
+          src="https://noithatduckhang.com/wp-content/uploads/2021/12/1-18.jpg"
+          alt="Dashboard"
+          className="w-full h-full object-cover"
+        />
       </div>
-      <div className="login-container">
+
+      {/* Right Section */}
+      <div className="login-container w-full md:w-1/2 p-8">
         <div className="login-box">
-          <div className="header">Xin chào,</div>
-          <div className="hi">Chào mừng bạn quay trờ lại!</div>
+          <div className="header text-2xl font-bold text-blue-600 mb-2">
+            Xin chào,
+          </div>
+          <div className="hi text-blue-500 mb-6">
+            Chào mừng bạn quay trở lại!
+          </div>
           <div className="form">
-            <div className="items">
-              <div className="name">Tài khoản</div>
-              <div className="value">
-                <input
+            <div className="items mb-4">
+              <div className="name block text-sm font-medium text-gray-700">
+                Tài khoản
+              </div>
+              <div className="value mt-1 block w-70 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-gray-500 sm:text-sm">
+                <Input
                   type="text"
+                  id="username"
+                  className="input-login outline-none"
                   value={username}
                   onChange={(e) => setUsername(e.target.value)}
                   required
@@ -103,11 +119,15 @@ const Login = () => {
                 />
               </div>
             </div>
-            <div className="items">
-              <div className="name">Mật khẩu</div>
-              <div className="value">
-                <input
+            <div className="items mb-4">
+              <div className="name block text-sm font-medium text-gray-700">
+                Mật khẩu
+              </div>
+              <div className="value w-70 border-2 border-gray-300 rounded-md">
+                <Input.Password
                   type="password"
+                  id="password"
+                  className="password-login block w-70"
                   value={password}
                   ref={passwordRef}
                   onChange={(e) => setPassword(e.target.value)}
@@ -121,16 +141,30 @@ const Login = () => {
             </div>
           </div>
           <div className="login-btn">
-            <Button id="login-btn" onClick={handleLogin} loading={loading}>
+            <Button
+              className="w-70 bg-blue-700 hover:bg-blue-700 text-white font-medium py-2 px-4 rounded-md border-2 border-black-700 transition duration-200"
+              id="login-btn"
+              onClick={handleLogin}
+              loading={loading}
+            >
               Đăng nhập
             </Button>
           </div>
           <div className="with-zalo">
-            <button>Đăng nhập bằng Zalo</button>
+            <button className="w-70 mt-4 bg-white border-1 border-blue-500  hover:bg-blue-200 hover:text-white-700 text-blue-600 font-medium text-1xl py-2 px-4 rounded-md transition duration-200 cursor-pointer">
+              Đăng nhập bằng Zalo
+            </button>
           </div>
           <div className="fc g4">
-            <div className="flex register">Đăng ký tài khoản mới</div>
-            <div className="flex forget-pass">Quên mật khẩu?</div>
+            <div className="flex register hover:underline mt-6 flex justify-between text-sm text-blue-600 cursor-pointer">
+              Đăng ký tài khoản mới
+            </div>
+            <div
+              className="flex forget-pass hover:underline cursor-pointer"
+              href="#"
+            >
+              Quên mật khẩu?
+            </div>
             <div className="flex">
               <Popconfirm
                 title="Thoát"
@@ -139,7 +173,9 @@ const Login = () => {
                   api.send("exit");
                 }}
               >
-                <div className="exit">Thoát</div>
+                <div className="exit mt-1 text-sm text-gray-600 hover:underline cursor-pointer">
+                  Thoát
+                </div>
               </Popconfirm>
             </div>
           </div>
