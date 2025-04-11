6088454bcb172fbb7c62538e1164d715ba0f2e8c
bubu_diep <hlitdvp98@gmail.com>
2025-04-11 16:51:40 +0700

    update account

--- src/page/company/company_accounts.jsx
+++ src/page/company/company_accounts.jsx
@@ -6,7 +6,6 @@ import { useUser } from "../../components/context/userContext";
 import Locked from "../../components/layout/Locked";
 import api from "../../components/api";
 import { FaLock, FaPlus } from "react-icons/fa";
-import { MdLockReset } from "react-icons/md";
 import { TbAlertSquareRoundedFilled, TbLockPause } from "react-icons/tb";
 import app from "../../components/app";
 
@@ -15,22 +14,97 @@ const Company_accounts = () => {
   const { user } = useUser();
   const [locked, setLocked] = useState(true);
   const [accounts, setAccounts] = useState([]);
+  const [isAdmin, setIsAdmin] = useState(false);
+  const [isSuper, setIsAdminSuper] = useState(false);
+
   const loadAccounts = () => {
-    api
-      .get("/accounts/", user.token)
-      .then((res) => {
-        if (res.results) {
-          setLocked(false);
-          setAccounts(res.results);
-        }
-      })
-      .catch((e) => {})
-      .finally(() => {});
+    api.get("/accounts/", user.token).then((res) => {
+      if (res.results) {
+        setLocked(false);
+        setAccounts(res.results);
+      }
+    });
   };
-  const addAccount = () => {};
+
   useEffect(() => {
+    setIsAdminSuper(user?.info?.isSuperAdmin);
+    setIsAdmin(user?.info?.isAdmin);
     loadAccounts();
   }, []);
+
+  const renderRole = (acc) =>
+    acc.Ban
+      ? "Đã ban"
+      : acc.isSuperAdmin
+      ? "Giám đốc"
+      : acc.isAdmin
+      ? "Quản lý"
+      : "Nhân viên";
+
+  const renderTools = (acc) => {
+    const canEdit =
+      (isSuper && !acc.isSuperAdmin) ||
+      (isAdmin && !acc.isAdmin && !acc.isSuperAdmin);
+    return canEdit ? (
+      <>
+        <Tooltip title="Lock: Chặn đăng nhập">
+          <Button
+            className="!text-[#0003] hover:!text-[#6c8eff]"
+            icon={<FaLock />}
+          />
+        </Tooltip>
+        <Tooltip title="Reset: Đặt lại mật khẩu">
+          <Button
+            className="!text-[#0003] hover:!text-[#6c8eff]"
+            icon={<TbLockPause />}
+          />
+        </Tooltip>
+      </>
+    ) : (
+      <Tooltip
+        title="Bị hạn chế do là tài khoản admin hoặc giám đốc"
+        className="text-[#f59d4b] flex gap-1 items-center text-[12px] font-[500]"
+      >
+        <TbAlertSquareRoundedFilled size={18} /> Hạn chế
+      </Tooltip>
+    );
+  };
+
+  const renderAccount = (acc) => (
+    <div
+      key={acc.id}
+      className="flex gap-2 items-center p-2 py-1.5 border-b-1 border-[#6a80ad33] relative hover:bg-[#f4f7fd] transition-all text-[13px]"
+    >
+      <div className="flex flex-col w-[50px] items-center">
+        <div className="avatar w-[40px] h-[40px] bg-[#0003] rounded-xl"></div>
+      </div>
+      <div className="flex flex-col w-[160px]">
+        <div className="name">Mã NV: {acc.cardID}</div>
+        <div className="name">Tài khoản: {acc.username}</div>
+      </div>
+      <div className="flex flex-col w-[180px]">
+        <div className="name">{renderRole(acc)}</div>
+        <div className="name">
+          Trạng thái: {acc.isActive ? "Hoạt động" : "Đã tắt"}
+        </div>
+      </div>
+      <div className="flex flex-col w-[120px]">
+        <div className="name">Sửa: {app.timeSince(acc.updated_at)}</div>
+        <div className="name">Tạo: {app.timeSince(acc.created_at)}</div>
+      </div>
+      <div className="tools flex gap-2 ml-auto mr-2">{renderTools(acc)}</div>
+    </div>
+  );
+  const filterOption = {
+    "Tài khoản": [
+      { value: "all", label: "Tất cả" },
+      { value: "SuperAdmin", label: "Boss" },
+      { value: "Admin", label: "Admin" },
+      { value: "Staff", label: "Staff" },
+    ],
+    "Phòng ban": [{ value: "all", label: "Tất cả" }],
+    "Chức vụ": [{ value: "all", label: "Tất cả" }],
+  };
   return (
     <div className="flex flex-1 overflow-hidden flex-col contacts-page">
       <div className="whiteTitle fadeInBot">
@@ -39,133 +113,42 @@ const Company_accounts = () => {
           {menu.label}
         </div>
       </div>
+
       <div className="flex flex-col flex-1 p-2 gap-2 fadeInTop">
         <div className="flex whitebox !shadow-none !p-0 text-[#d84f00] border-[#d84f00] border-1">
-          <div className="flex icon p-3 border-r-1">
+          <div className="icon p-3 border-r-1">
             <GoAlertFill />
           </div>
           <div className="flex items-center ml-2 font-[500]">
             Chỉ admin mới được thêm mới tài khoản
           </div>
         </div>
+
         <div className="whitebox h-full flex flex-col !p-0">
           {!locked ? (
             <>
               <div className="flex p-2 border-b-1 border-[#0002] gap-2">
-                <Select
-                  defaultValue="all"
-                  placeholder="Phân loại tài khoản"
-                  className="min-w-[150px]"
-                >
-                  <Select.Option value="all">Tất cả tài khoản</Select.Option>
-                  <Select.Option value="SuperAdmin">SuperAdmin</Select.Option>
-                  <Select.Option value="Admin">Admin</Select.Option>
-                  <Select.Option value="User">User</Select.Option>
-                </Select>
-                <Select
-                  defaultValue="all"
-                  placeholder="Phòng ban"
-                  className="min-w-[160px]"
-                >
-                  <Select.Option value="all">Tất cả phòng ban</Select.Option>
-                </Select>
-                <Select
-                  defaultValue="all"
-                  placeholder="Chức vụ"
-                  className="min-w-[160px]"
-                >
-                  <Select.Option value="all">Tất cả chức vụ</Select.Option>
-                </Select>
+                {["Tài khoản", "Phòng ban", "Chức vụ"].map((label, idx) => (
+                  <Select
+                    key={idx}
+                    defaultValue="all"
+                    placeholder={label}
+                    className="min-w-[160px]"
+                    options={filterOption[label]}
+                  />
+                ))}
                 <Button
                   type="primary"
                   className="ml-auto"
                   icon={<FaPlus />}
-                  onClick={addAccount}
+                  onClick={() => {}}
                 >
                   Thêm tài khoản
                 </Button>
               </div>
+
               <div className="mr-0.5 my-0.5 flex flex-col overflow-y-auto px-1">
-                {accounts.map((acc) => (
-                  <div
-                    key={acc.id}
-                    className="flex gap-2 items-center p-2 py-1.5 border-b-1 duration-300
-                  border-[#6a80ad33] relative hover:bg-[#f4f7fd] transition-all text-[13px]"
-                  >
-                    <div className="flex flex-col w-[50px] items-center">
-                      <div className="avatar w-[40px] h-[40px] bg-[#0003] rounded-xl"></div>
-                    </div>
-                    <div className="flex flex-col w-[160px]">
-                      <div className="flex name">Mã NV: {acc.cardID}</div>
-                      <div className="flex name">Tài khoản: {acc.username}</div>
-                    </div>
-                    <div className="flex flex-col w-[180px]">
-                      <div className="flex name">
-                        {acc.Ban
-                          ? "Đã ban"
-                          : acc.isSuperAdmin
-                          ? "Giám đốc"
-                          : acc.isAdmin
-                          ? "Admin"
-                          : "Nhân viên"}
-                      </div>
-                      <div className="flex name">
-                        Trạng thái:
-                        {acc.isActive ? " Hoạt đông" : " Đã tắt"}
-                      </div>
-                    </div>
-                    <div className="flex flex-col w-[120px]">
-                      <div className="flex name">
-                        Sửa: {app.timeSince(acc.updated_at)}
-                      </div>
-                      <div className="flex name">
-                        Tạo: {app.timeSince(acc.created_at)}
-                      </div>
-                    </div>
-                    <div className="tools flex gap-2 ml-auto mr-2">
-                      {user.info.isSuperAdmin && !acc.isSuperAdmin ? (
-                        <>
-                          <Tooltip title="Lock: Chặn đăng nhập">
-                            <Button
-                              className="!text-[#0003] hover:!text-[#6c8eff]"
-                              icon={<FaLock />}
-                            />
-                          </Tooltip>
-                          <Tooltip title="Reset: Đặt lại mật khẩu">
-                            <Button
-                              className="!text-[#0003] hover:!text-[#6c8eff]"
-                              icon={<TbLockPause />}
-                            />
-                          </Tooltip>
-                        </>
-                      ) : !acc.isAdmin &&
-                        !acc.isSuperAdmin &&
-                        user.info.isAdmin ? (
-                        <>
-                          <Tooltip title="Lock: Chặn đăng nhập">
-                            <Button
-                              className="!text-[#0003] hover:!text-[#6c8eff]"
-                              icon={<FaLock />}
-                            />
-                          </Tooltip>
-                          <Tooltip title="Reset: Đặt lại mật khẩu">
-                            <Button
-                              className="!text-[#0003] hover:!text-[#6c8eff]"
-                              icon={<TbLockPause />}
-                            />
-                          </Tooltip>
-                        </>
-                      ) : (
-                        <Tooltip
-                          title="Bị hạn chế do là tài khoản admin hoặc giám đốc"
-                          className="text-[#f59d4b] flex gap-1 items-center text-[12px] font-[500]"
-                        >
-                          <TbAlertSquareRoundedFilled size={18} /> Hạn chế
-                        </Tooltip>
-                      )}
-                    </div>
-                  </div>
-                ))}
+                {accounts.map(renderAccount)}
               </div>
             </>
           ) : (
