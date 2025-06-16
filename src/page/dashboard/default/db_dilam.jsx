95f804c1e46cf4d1c1cbe1a4f2657a5570f8b287
Bubudiep <hlitdvp98@gmail.com>
2025-06-16 22:40:37 +0700

    cc

--- src/page/dashboard/default/db_dilam.jsx
+++ src/page/dashboard/default/db_dilam.jsx
@@ -1,12 +1,53 @@
 import { Empty, Tooltip } from "antd";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { FaInfoCircle } from "react-icons/fa";
+import ReactApexChart from "react-apexcharts";
 
 const Db_dilam_card = ({ user }) => {
+  const [chartData, setChartData] = useState({ labels: [], values: [] });
+  useEffect(() => {
+    const labels = Object.keys(user?.company?.Dashboard?.op?.by_customer);
+    const values = Object.values(user?.company?.Dashboard?.op?.by_customer);
+    const data = {
+      labels: labels,
+      values: values,
+    };
+    const timeout = setTimeout(() => {
+      setChartData(data);
+    }, 300);
+
+    return () => clearTimeout(timeout);
+  }, []);
+  const chartOptions = {
+    chart: {
+      type: "bar",
+      height: 500,
+      toolbar: { show: false },
+    },
+    plotOptions: {
+      bar: {
+        borderRadius: 4,
+        horizontal: false,
+        columnWidth: "50%",
+      },
+    },
+    dataLabels: { enabled: false },
+    xaxis: {
+      categories: chartData.labels,
+      labels: { style: { fontSize: "13px" } },
+    },
+    colors: ["#1890ff"],
+  };
+  const chartSeries = [
+    {
+      name: "Đang đi làm",
+      data: chartData.values,
+    },
+  ];
   return (
     <div className="flex flex-col flex-1">
       <div className="text-[15px] text-[#666] font-[500] flex justify-between">
-        Thống kê đi làm
+        Thống kê nhân lực theo nhà chính
         <Tooltip
           color="white"
           title={
@@ -20,8 +61,14 @@ const Db_dilam_card = ({ user }) => {
           </div>
         </Tooltip>
       </div>
-      <div className="flex flex-1 items-center justify-center">
-        <Empty description="Đang phát triển!" />
+
+      <div className="flex-1 mt-4">
+        <ReactApexChart
+          options={chartOptions}
+          series={chartSeries}
+          type="bar"
+          height={350}
+        />
       </div>
     </div>
   );
