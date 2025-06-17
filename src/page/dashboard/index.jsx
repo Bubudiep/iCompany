1b76c0dbc7d27c22663e7f4f6848e9de6b48b93f
bubu_diep <hlitdvp98@gmail.com>
2025-06-17 09:47:05 +0700

    cc

--- src/page/dashboard/index.jsx
+++ src/page/dashboard/index.jsx
@@ -11,7 +11,7 @@ import { FaChartSimple, FaMoneyBillTransfer } from "react-icons/fa6";
 const Dashboard_index = () => {
   const { user } = useUser();
   return (
-    <div className="flex flex-1 overflow-hidden">
+    <div className="flex flex-1 overflow-hidden dashboard">
       <div className="flex flex-1">
         <div className="flex flex-col gap-4 flex-1 overflow-y-auto p-2">
           <div className="flex gap-4 fadeInTop">
@@ -25,16 +25,16 @@ const Dashboard_index = () => {
                 <div className="text-[15px] text-[#666] font-[500] flex justify-between">
                   Top tuyển dụng
                 </div>
-                <div className="text-[13px] text-[#999] flex gap-1 items-center">
-                  <FaChartSimple className="text-[16px] mt-0.5" />
-                  Top 4 nhân viên nhiều nhân lực đi làm nhất
+                <div className="text-[13px] text-[#999] flex gap-1.5 items-center mt-1">
+                  <FaChartSimple className="text-[14px]" />
+                  Top nhân viên nhiều nhân lực nhất
                 </div>
                 <div className="flex mt-auto gap-1">
                   <div className="flex w-[50%] flex-col bg-[#e2f2ff] text-[#008cff] rounded-[4px] px-2 py-1">
-                    <div className="flex px-1 py-1 font-[500]">TOP 1</div>
-                    <div className="flex flex-1 flex-col items-center justify-center text-[40px] font-[500] pb-5">
+                    <div className="flex px-1 py-1 font-[700]">TOP 1</div>
+                    <div className="flex flex-1 flex-col items-center justify-center text-[32px] font-[500] pb-5">
                       {user?.company?.Dashboard?.op?.by_nguoituyen?.[0]?.total}
-                      <div className="text-[14px]">
+                      <div className="text-[13px]">
                         {
                           <Staff_view
                             className="mt-2"
@@ -47,26 +47,29 @@ const Dashboard_index = () => {
                       </div>
                     </div>
                   </div>
-                  <div className="flex flex-1 flex-col">
+                  <div className="flex flex-1 flex-col gap-1">
                     {user?.company?.Dashboard?.op?.by_nguoituyen?.map(
                       (op, idx) => {
                         return (
                           idx > 0 && (
                             <div
                               key={op?.nguoituyen__id}
-                              className="justify-between flex flex-col px-2 py-1 bg-[#d1f6ff] rounded-[4px]"
+                              className="justify-between text-[13px] flex flex-col px-2 py-1 bg-[#d1f6ff] rounded-[4px]"
                             >
-                              <div className="flex flex-1 font-[700] text-[10px] gap-1">
+                              <div className="flex flex-1 font-[600] text-[10px]">
                                 TOP {idx + 1}
-                              </div>
-                              <div className="flex justify-between">
-                                <Staff_view id={op?.nguoituyen__id} />
-                                <div className="text-[#000] font-[500] flex gap-0.5 items-baseline">
+                                <div className="text-[#000000b9] font-[500] flex gap-0.5 items-baseline ml-auto">
                                   {op?.total}{" "}
-                                  <div className="font-[400] text-[11px]">
+                                  <div className="font-[400] text-[#00000075]">
                                     người
                                   </div>
                                 </div>
+                              </div>
+                              <div className="flex justify-between">
+                                <Staff_view
+                                  className="whitespace-nowrap text-[12px] overflow-hidden"
+                                  id={op?.nguoituyen__id}
+                                />
                               </div>
                             </div>
                           )
